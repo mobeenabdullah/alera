5756c7b3e1c65e8f7dfa13e65207b79aec20b689
Mobeen Abdullah <mobeenabdullah@gmail.com>
2020-08-03 15:44:01 +0500

    removed custom background support

--- functions.php
+++ functions.php
@@ -60,12 +60,6 @@ if ( ! function_exists( 'alera_setup' ) ) :
 			'caption',
 		) );
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'alera_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
-		) ) );
-
 		// Add theme support for selective refresh for widgets.
 		add_theme_support( 'customize-selective-refresh-widgets' );
 
