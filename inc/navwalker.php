48d4ecc15d3ec6bc062913da617466db1a2970f1
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-04-09 22:53:22 +0500

    fix menu issue

--- inc/navwalker.php
+++ inc/navwalker.php
@@ -9,52 +9,48 @@
  * Class Name: Navwalker
  * Plugin Name: Bulma Navwalker
  * Plugin URI:  https://github.com/Poruno/Bulma-Navwalker
- * Description: An extended WordPress Navwalker object that displays Bulma framework's Navbar https://bulma.io/ in WordPress.
+ * Description: An extended Wordpress Navwalker object that displays Bulma framework's Navbar https://bulma.io/ in Wordpress.
  * Author: Carlo Operio - https://www.linkedin.com/in/carlooperio/, Bulma-Framework
  * Author URI: https://github.com/wp-bootstrap
  * License: GPL-3.0+
  * License URI: https://github.com/Poruno/Bulma-Navwalker/blob/master/LICENSE
  */
 
-	class Navwalker extends Walker_Nav_Menu {
+class Navwalker extends Walker_Nav_Menu {
 
-        public function start_lvl( &$output, $depth = 0, $args = array() ) {
-           
-            $output .= "<div class='navbar-dropdown'>";
-        }
+	public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
 
-        public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+		$lowerCaseTitle = strtolower($item->title);
+		$linkTitle = str_replace(' ', '', $lowerCaseTitle);
+		$liClasses = 'navbar-item '.$linkTitle;
 
-            $liClasses = 'navbar-item '.$item->title;
+		$hasChildren = $args->walker->has_children;
+		$liClasses .= $hasChildren ? " has-dropdown is-hoverable": "";
 
-            $hasChildren = $args->walker->has_children;
-            $liClasses .= $hasChildren? " has-dropdown is-hoverable": "";
+		$linkClasses = $hasChildren ? "" : "is-arrowless";
 
-            if($hasChildren){
-                $output .= "<div class='".$liClasses."'>";
-                $output .= "\n<a class='navbar-link' href='".$item->url."'>".$item->title."</a>";
-            }
-            else {
-                $output .= "<a class='".$liClasses."' href='".$item->url."'>".$item->title;
-            }
+		$output .= '<li class="'.$liClasses.'">';
 
-            // Adds has_children class to the item so end_el can determine if the current element has children
-            if ( $hasChildren ) {
-                $item->classes[] = 'has_children';
-            }
-        }
-        
-        public function end_el(&$output, $item, $depth = 0, $args = array(), $id = 0 ){
+		$output .= '<a class="navbar-link '.$linkClasses.'" href="'.$item->url.'">'.$item->title.'</a>';
 
-            if(in_array("has_children", $item->classes)) {
+		// Adds has_children class to the item so end_el can determine if the current element has children
+		if ( $hasChildren ) {
+			$item->classes[] = "has_children";
+		}
+	}
 
-                $output .= "</div>";
-            }
-            $output .= "</a>";
-        }
+	public function start_lvl( &$output, $depth = 0, $args = array() ) {
+		$output .= '<ul class="navbar-dropdown">';
+	}
 
-        public function end_lvl (&$output, $depth = 0, $args = array()) {
+	public function end_lvl (&$output, $depth = 0, $args = array()) {
+		$output .= "</ul>";
+	}
 
-            $output .= "</div>";
-        }
-    }
+	public function end_el(&$output, $item, $depth = 0, $args = array(), $id = 0 ){
+
+		$output .= "</li>";
+
+	}
+
+}
